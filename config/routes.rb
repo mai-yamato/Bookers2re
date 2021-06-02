ca083fe21858adfeafee159a8bb2a604695a59d7
mai-yamato <my512.1515@gmail.com>
2021-06-02 11:09:54 +0000

    kadai3first

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :users,only: [:show, :index, :edit, :update]
-  resources :books,only: [:show, :index, :edit, :update, :destroy, :create]
+  resources :books,only: [:show, :index, :edit, :update, :destroy, :create] do
+    resource :favorites,only: [:create, :destroy]
+  end
   get "/home/about" => "homes#about"
 end
