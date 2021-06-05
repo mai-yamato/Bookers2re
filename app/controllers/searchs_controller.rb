bd3d8a01e73e6594c1ed69b490538699a5fb6ea5
mai-yamato <my512.1515@gmail.com>
2021-06-05 14:26:34 +0000

    課題５完成

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,28 +1,17 @@
 class SearchsController < ApplicationController
 
-  def search
-    @model = params["model"]
-    @content = params["content"]
-    @method = params["method"]
-    @records = search_for(@model, @content, @method)
-  end
 
-  private
-  def search_for(model, content, method)
-    if model == 'User'
-      if method == 'perfect'
-        User.where(name: content)
-      else
-        User.where('name LIKE ?', '%'+content+'%')
-      end
-    elsif model == 'Book'
-      if method == 'perfect'
-        Post.where(title: content)
-      else
-        Post.where('title LIKE ?', '%'+content+'%')
-      end
+before_action :authenticate_user!
+
+  def search
+    @word = params[:word]
+    @range = params[:range]
+
+    if @range == "User"
+      @users = User.looks(params[:search], params[:word])
+    else
+      @books = Book.looks(params[:search], params[:word])
     end
   end
-
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,8 @@ class UsersController < ApplicationController
    end
 
 
+
+
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
